0e781d591bb3b537a56e9069d22105b184c59187
Harsh V Bhandari <harsh.bhandari.v@gmail.com>
2021-06-08 21:30:32 +0530

    production setup 1

--- backend/server.js
+++ backend/server.js
@@ -2,33 +2,41 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const mongoose = require('./config/mongooseConfig');
+const mongoose = require("./config/mongooseConfig");
 
 //body parser middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const morgan = require('morgan');
-app.use(morgan('dev'));
+const morgan = require("morgan");
+app.use(morgan("dev"));
 
-require('./config/passportConfig');
-const sessionParams = require('./config/sessionConfig');
-const passport = require('passport');
-const session = require('express-session');
+require("./config/passportConfig");
+const sessionParams = require("./config/sessionConfig");
+const passport = require("passport");
+const session = require("express-session");
 app.use(session(sessionParams));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get("/", function (req, res) {
-  res.send("Hello" + req.user);
-});
+if (process.env.NODE_ENV === "devolopment")
+  app.get("/", function (req, res) {
+    res.send("Hello" + req.user);
+  });
 
-const auth = require('./routes/auth');
-const userRoute = require('./routes/userRoute');
-const formRoute = require('./routes/formRoute');
-app.use('/auth', auth);
-app.use('/api/user', userRoute);
-app.use('/api/form', formRoute);
+const auth = require("./routes/auth");
+const userRoute = require("./routes/userRoute");
+const formRoute = require("./routes/formRoute");
+app.use("/auth", auth);
+app.use("/api/user", userRoute);
+app.use("/api/form", formRoute);
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 3500;
 app.listen(PORT, function () {
